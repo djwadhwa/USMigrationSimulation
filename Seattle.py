11b78237c5086d14241158bb7c01f4ebbf5d4482
DJ <wadhwadhananjay@gmail.com>
2019-05-28 14:41:36 -0700

    additional initialization

--- Seattle.py
+++ Seattle.py
@@ -1,7 +1,23 @@
 import migration_factors as mf
 import city_factors as cf
-
+import random as rnd
 #CONSTANTS
 SEATTLE_STARTING_POPULATION = 724745
+food_needed  = 0
+water_needed = 0
+class Seattle:
+    def __init__(self, job_rate, crime_rate, cost_of_living):
+        
+    
+    def setMigration():
+        
+    def getFood():
+        
+        return food_needed
+        
+    def getWater():
+        return water_needed
+        
+        
 
 #=========
--- simulation.py
+++ simulation.py
@@ -1,1 +1,17 @@
 #simulation
+import random as rnd
+
+def calcMigrationFactors(total_population):
+    if (job_rate <= 10):
+        
+    return job_rate*1/crimerate+1/cost_of_living*education
+    
+timestep = 20
+initPop = 724745
+wealth_dist = "normal"
+age_dist = "normal"
+job_rate = 
+
+for i in range (timestep):
+    
+    
