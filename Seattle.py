2c0e488e12ba24ea0fc4ea0d75deee2210fe947a
djwadhwa <wadhwadhananjay@gmail.com>
2019-05-28 21:57:15 -0700

    add changes to simulation and seattle

--- Seattle.py
+++ Seattle.py
@@ -1,4 +1,4 @@
 population = 724745
 jobs =  506880
-children = population *0.202
-adults = population - children
+children = 0.153
+adults = 1- children
--- simulation.py
+++ simulation.py
@@ -6,7 +6,9 @@ deltaTime = 1
 totalTime = 20
 
 def age_dist(population, children, adults):
+    randNum = rnd.uniform(-1,1)
+    print(randNum)
     
-
-    
-    
+#test    
+for i in range (10):   
+    age_dist(sea.population, sea.children, sea.adults)
