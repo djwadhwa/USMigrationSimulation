d0349fd7c8452e6eac56c2638d8e1168aaa586c1
djwadhwa <wadhwadhananjay@gmail.com>
2019-05-28 22:33:56 -0700

    Update simulation.py

--- simulation.py
+++ simulation.py
@@ -13,15 +13,15 @@ def age_dist(population, children, adults):
 # Function returns an array of the child age distributions at each phase of the simulation
 # Child age dist should be between 0.13 and 0.25
 def childAgeDist(initialChildrenValue):
-    randNums = N.random.uniform(-0.01, 0.01, int(totalTime / deltaTime))
+    randNums = N.random.uniform(-0.02, 0.02, int(totalTime / deltaTime))
     retVal = N.zeros(int(totalTime / deltaTime))
     retVal[0] = initialChildrenValue
     # For each year, update the child age dist
     for year in range(1, int(totalTime / deltaTime)):
         temp = retVal[year-1] + randNums[year-1]
         # Keep child age dist between 0.13 and 0.25
-        temp = max(temp, 13)
-        temp = min(temp, 25)
+        temp = max(temp, .13)
+        temp = min(temp, .25)
         retVal[year] = temp
     print(retVal)
     return retVal
