4b0c5e8397940e2449479cfb5ba5ccc70c458ae2
Gahl Goziker <gahl.goziker@gmail.com>
2019-05-28 22:17:09 -0700

    Add childAgeDist function to simulation.py

--- Seattle.py
+++ Seattle.py
@@ -1,4 +1,4 @@
 population = 724745
 jobs =  506880
-children = 0.153
-adults = 1- children
+children = 0.153        # Portion of population which is under 18 y.o.
+adults = 1- children    # Portion of population which is over 18 y.o.
--- simulation.py
+++ simulation.py
@@ -1,6 +1,7 @@
 #simulation
 import random as rnd
 import Seattle as sea
+import numpy as N
 
 deltaTime = 1
 totalTime = 20
@@ -8,6 +9,15 @@ totalTime = 20
 def age_dist(population, children, adults):
     randNum = rnd.uniform(-1,1)
     print(randNum)
+
+# Function returns an array of the child age distributions at each phase of the simulation
+def childAgeDist(initialPopulation, children, adults):
+    randNums = rnd.uniform(-1, 1, totalTime / deltaTime-1)
+    retVal = N.zeros(totalTime / deltaTime)
+    retVal[0] = initialPopulation
+    for year in range(1, totalTime/deltaTime):
+        retVal[year] = retVal[year-1] + randNums[year-1]
+    print(retVal)
     
 #test    
 for i in range (10):   
