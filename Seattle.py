47990cfa9b9e952851b5e33c60738b6fe9312e69
djwadhwa <wadhwadhananjay@gmail.com>
2019-05-28 20:56:56 -0700

    changed Seattle.py to only contain constants

--- Seattle.py
+++ Seattle.py
@@ -1,23 +1,4 @@
-import migration_factors as mf
-import city_factors as cf
-import random as rnd
-#CONSTANTS
-SEATTLE_STARTING_POPULATION = 724745
-food_needed  = 0
-water_needed = 0
-class Seattle:
-    def __init__(self, job_rate, crime_rate, cost_of_living):
-        
-    
-    def setMigration():
-        
-    def getFood():
-        
-        return food_needed
-        
-    def getWater():
-        return water_needed
-        
-        
-
-#=========
+population = 724745
+jobs =  506880
+children = population *0.202
+adults = population - children
