a5a39eb72d75607e4c675133065c939ff506fe38
DJ <wadhwadhananjay@gmail.com>
2019-06-02 15:58:37 -0700

    added plotter function

--- simulation.py
+++ simulation.py
@@ -16,25 +16,11 @@ def age_dist(prev_child_percentage, lower_bound = 0.13, upper_bound = 0.25):
     
     # Keep child age dist between 0.13 and 0.25
     temp = prev_child_percentage + rand
-    temp = max(temp, lower_bound)
-    temp = min(temp, upper_bound)
+    # temp = max(temp, lower_bound)
+    # temp = min(temp, upper_bound)
     
     # Function returns an array of the child age distributions at each phase of the simulation  
     return temp
-
-# # Poverty rate should be between 0.09 and 0.23
-# def povertyRate(initialPovertyRate):
-#     randNums = rnd.uniform(-0.02, 0.02, time)
-#     retVal = N.zeros(time)
-#     retVal[0] = initialPovertyRate
-#     # For each year, update the poverty rate
-#     for year in range(1, time):
-#         temp = retVal[year-1] + randNums[year-1]
-#         # Keep poverty rate between 0.09 and 0.23
-#         temp = max(temp, .09)
-#         temp = min(temp, .23)
-#         retVal[year] = temp
-#     return retVal
 
 def natural_pop_growth (population):
     delta_natural_pop = population * rnd.uniform \
@@ -45,7 +31,7 @@ def natural_pop_growth (population):
     return total_natural_pop
     
 # Job distribution increases at about 2% to 2.5% a year.
-def jobs(jobs, job_rate):
+def update_jobs(jobs, job_rate):
     lower_bound = job_rate[0];
     upper_bound = job_rate[1];
     
@@ -55,7 +41,7 @@ def jobs(jobs, job_rate):
     
     return total_jobs
 
-def crimes(crimes, crime_rate):
+def update_crimes(crimes, crime_rate):
     lower_bound = crime_rate[0];
     upper_bound = crime_rate[1];
     
@@ -65,7 +51,7 @@ def crimes(crimes, crime_rate):
     
     return total_crimes
 
-def rent(rent, rent_rate):
+def update_rent(rent, rent_rate):
     lower_bound = rent_rate[0];
     upper_bound = rent_rate[1];
     
@@ -75,35 +61,61 @@ def rent(rent, rent_rate):
     
     return total_rent
     
-def taxes(taxes, tax_rate):
+def update_taxes(taxes, tax_rate):
     lower_bound = tax_rate[0];
     upper_bound = tax_rate[1];
     
     random_taxes = N.random.uniform(lower_bound ,upper_bound)
     rand_delta = taxes * random_taxes
-    total_taxes = int(taxes + rand_delta)
+    total_taxes = taxes + rand_delta
     
     return total_taxes
 
-# Return number of litres of water that would be drunk annually by a population of the
-# indicated size, with the indicated distribution of children and adults
+# Return number of litres of water that would be drunk annually by a population
+# of theindicated size, with the indicated distribution of children and adults
 def water_consumption(population, adult_dist):
     daily_water_children= ( 7 + 10 + 14 ) / 3 * 0.236 * 0.264172     #in gallons
     daily_water_adults= 3.7 * 0.264172                           #in gallons
     
     annual_water_adults = daily_water_adults * (population * adult_dist) * 365
-    annual_water_children = daily_water_children * (population * 1 - adult_dist) * 365
+    annual_water_children = daily_water_children * \
+    (population * 1 - adult_dist) * 365
     total_annual_water = annual_water_adults + annual_water_children
     
-    return (annual_water_children, annual_water_adults, total_annual_water)
+    # return (annual_water_children, annual_water_adults, total_annual_water)
+    return total_annual_water
 
 def food(population, child_rate):
     return 0
     
-def calculate_migrants(free_jobs, crimes, rent, taxes):
-    return free_jobs * 1/crimes * rent *taxes
     
-def main(city, time):
+def plotter (city, popualtion_array, water_array, time_array):
+    fig1, ax1 = plt.subplots()
+    ax1.plot(time_array, popualtion_array)
+    ax1.plot (N.arange(14), city.pop_list)
+    ax1.set_title(city.Name + "'s population growth due to migration over "+  
+    str(len(time_array))+" years")
+    ax1.set_xlabel("Time (years)")
+    ax1.set_ylabel("Population")
+    # fig2, ax1 = plt.subplots()
+    # ax1.plot(time_array, water_array)
+    # ax1.set_title(city.Name + "'s water consumption over "+  
+    # str(len(time_array))+" years")
+    # ax1.set_xlabel("Time (years)")
+    # ax1.set_ylabel("Water consumed (100 million gallons)")
+    plt.show()
+    
+def printer(city, population_array, time_array):
+    print (city.Name+"s poulation over " + str(len(time_array)) + "years")
+    for year in time_array:
+        print (2006+year, "\t", population_array[year] )
+
+def calculate_migrants(free_jobs, crimes, rent, taxes):
+    # return free_jobs * 1/crimes * rent *taxes
+    # return (0.7*free_jobs - 0.6*crimes - 7*rent - 30000*taxes)
+    return free_jobs - N.log(crimes+rent*1/taxes)
+    
+def main(city, time = 20, trials = 100):
     
     adult_dist = city.adults
     adults = city.adults*city.population
@@ -114,46 +126,37 @@ def main(city, time):
     rent = city.rent
     taxes = city.taxes
     
-    population_array = N.zeros(time);
+    # print (population)
     
-    for i in range (time):
-        free_jobs = (1-total_jobs/adults)*city.population
-        migrants = calculate_migrants(free_jobs, crimes, rent, taxes)
-        # population = natural_pop_growth(population) 
-        population += migrants
+    time_array = N.arange(time)
+    population_array = N.zeros(time)
+    water_array = N.zeros(time)
+    population_array[0] = city.population
+    
+    for year in range (1, time):
         
+        free_jobs = total_jobs*.174+ total_jobs-adults
+        # print(free_jobs)
+        migrants = calculate_migrants(free_jobs, crimes, rent, taxes)
+        population_array[year] = natural_pop_growth(population_array[year-1]) 
+        population_array[year] += migrants
+        # print (migrants / population)
+        #output
+        water_array[year] = int (water_consumption(population, adult_dist))
+        
+        #update every year
         adult_dist = 1 - age_dist(1-adult_dist)
         adults = adult_dist * population
-        total_jobs = jobs(total_jobs, city.job_range)
-        population_array[i] = int(population)
+        total_jobs = update_jobs(total_jobs, city.job_range)
+        crimes = update_crimes(crimes,city.crimes_range )
+        rent = update_rent(rent, city.rent_range)
+        taxes = update_taxes (taxes, city.taxes_range)
         
-    # plt.plot(N.arange(time), population_array)
-    print (population_array)
-    plt.plot(N.arange(time), population_array)
-    # plt.plot (N.arange(time), )
-    plt.show()
-
-
-# # Generates a plot, showing the population, unemployment rate, and poverty rate throughout the simulation
-# def plot(population, unemploymentRate, povertyRate):
-#     x = N.linspace(2018, 2018+totalTime, totalTime/deltaTime)
-# 
-#     plt.subplot(3, 1, 1)
-#     plt.plot(x, population, 'o-')
-#     plt.title('US Migration Simulator Lite')
-#     plt.ylabel('Population')
-# 
-#     plt.subplot(3, 1, 2)
-#     plt.plot(x, unemploymentRate, 'o-')
-#     plt.ylabel('Unemployment Rate')
-# 
-#     plt.subplot(3, 1, 3)
-#     plt.plot(x, povertyRate, 'o-')
-#     plt.xLabel('Year')
-#     plt.ylabel('Poverty Rate')
+    printer (city, population_array, time_array)
+    plotter (city, population_array, water_array, time_array)
 
 #test
 # print ("\nChild age distribution:\n", childAgeDist(sea.children)*100)
 # print ("\nPoverty rate :\n", povertyRate(sea.povertyRate)*100)
 # print ("\nJob distribution :\n", job_dist(sea.jobs))
-main(sea, 20)
+main(sea, 13)
