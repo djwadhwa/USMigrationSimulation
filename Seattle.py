2650d297869d0fc391988918373d394048a95e43
DJ <wadhwadhananjay@gmail.com>
2019-06-02 15:58:20 -0700

    added population and jobs

--- Seattle.py
+++ Seattle.py
@@ -1,17 +1,11 @@
 import csv
 import numpy as np
 
-population = 724745
-children = 0.153        # Portion of population under 18 y.o.
-adults = 1- children    # Portion of population over 18 y.o.
-povertyRate = 0.125
-
-jobs =  506880
-job_range = (.02, .025)
-
 crimes_list = []
 rent_list = []
 taxes_list = []
+pop_list = []
+jobs_list = []
 
 with open ("seattle_data.csv") as csv_file:
     read_csv = csv.reader (csv_file, delimiter =',')
@@ -20,15 +14,27 @@ with open ("seattle_data.csv") as csv_file:
         rent_list.append(int (row [1]))
         taxes_list.append(float(row [2]))
         crimes_list.append(int (row [3]))
+        pop_list.append(int (row[4]))
+        jobs_list.append(int(row[5]))
         
-rent = rent_list[-1]
+Name = "Seattle"
+population = pop_list[0]
+children = 0.153        # Portion of population under 18 y.o.
+adults = 1- children    # Portion of population over 18 y.o.
+povertyRate = 0.125
+
+jobs =  jobs_list[0]
+job_range = (.02, .025)
+print (pop_list)
+rent = rent_list[0]
 rent_std_percentage = np.std(rent_list[:-1])/np.mean(rent_list)
 rent_range = (-rent_std_percentage, rent_std_percentage)
 
-crimes = crimes_list[-1]
+crimes = crimes_list[0]
 crimes_std_percentage = np.std (crimes_list[:-1])/np.mean(crimes_list)
 crimes_range = (-crimes_std_percentage, crimes_std_percentage)
 
-taxes = taxes_list [-1]
+taxes = taxes_list[0]
 taxes_std_percentage = np.std(taxes_list [:-1])/np.mean(taxes_list)
 taxes_range = (0, taxes_std_percentage)
+print(taxes_list)
